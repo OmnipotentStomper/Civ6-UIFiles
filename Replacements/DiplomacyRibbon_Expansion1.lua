07d3550c5f1f90fa003f2b6c0017c85aaccc6f10
Antoine Lassier <toinousp@gmail.com>
2019-05-07 16:38:31 +0200

    Hypothesis update 1.0.0.13770

--- Replacements/DiplomacyRibbon_Expansion1.lua
+++ Replacements/DiplomacyRibbon_Expansion1.lua
@@ -11,9 +11,14 @@ BASE_AddLeader = AddLeader;
 -- ===========================================================================
 function AddLeader(iconName : string, playerID : number, isUniqueLeader: boolean)
 	local leaderIcon, instance = BASE_AddLeader(iconName, playerID, isUniqueLeader);
+	local localPlayerID:number = Game.GetLocalPlayer();
+
+	if localPlayerID == -1 or localPlayerID == 1000 then
+		return;
+	end
 
 	-- Update relationship pip tool with details about our alliance if we're in one
-	local localPlayerDiplomacy:table = Players[Game.GetLocalPlayer()]:GetDiplomacy();
+	local localPlayerDiplomacy:table = Players[localPlayerID]:GetDiplomacy();
 	if localPlayerDiplomacy then
 		local allianceType = localPlayerDiplomacy:GetAllianceType(playerID);
 		if allianceType ~= -1 then
